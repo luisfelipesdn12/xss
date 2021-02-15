252d3b0c413d76c1609e797e223057c509006bea
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2021-02-15 12:23:27 -0300

    Update hackerman.js

--- hackerman.js
+++ hackerman.js
@@ -1,3 +1,3 @@
 const main = () => {
-  console.log("Testing");
+  document.getElementById.innerHTML = `<a href="https://github.com/luisfelipesdn12/xss/edit/main/hackerman.js" target="_blank"><h3><i class="fab fa-github"></i> Ver projeto</h3></a><h3>by <a href="https://luisfelipesdn12.now.sh/" target="_blank">Luis Felipe</a></h3>`;
 }
